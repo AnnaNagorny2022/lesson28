f494241d16ac4686407a3d6124aa0e651ab28da9
AnnaNagorny2022 <a.melyukh@mail.ru>
2023-05-04 21:54:40 +0300

    Add task02 without inheritance

---
+++ task02/Doctor.py
@@ -0,0 +1,44 @@
+class Doktor:
+    def __init__(self, name='no name', age='0', alive=True, experience=1):
+        self.__name = name
+        self.__age = age
+        self.__alive = alive
+        self.__experience = experience
+
+    def can_cure(self):
+        print(self.__name + "can cure.")
+
+    @property
+    def experience(self):
+        return self.__experience
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str):
+            self.__name = name
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age=1):
+        if 0 < age <= 120:
+            self.__age = age
+
+    @property
+    def is_alive(self):
+        return "Yes" if self.__alive else "No"
+
+    @is_alive.setter
+    def is_alive(self, alive):
+        if isinstance(alive, bool):
+            self.__alive = alive
+
+    def __str__(self):
+        return f"{self.__name}: age = {self.__age}." \
+               f"Is alive? - {self.is_alive}"
